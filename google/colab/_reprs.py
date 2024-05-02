e082d9f02ee5aea7266dccbc219207c2a45d6f95
matt <matt@anthropic.com>
2024-05-02 15:39:14 -0700

    wip

--- google/colab/_reprs.py
+++ google/colab/_reprs.py
@@ -13,12 +13,6 @@ import IPython
 from IPython.core import oinspect
 import numpy as np
 import PIL as pil
-# pylint: disable=g-import-not-at-top
-# with warnings.catch_warnings():
-#   # Importing via IPython raises a spurious warning, but avoids a version
-#   # mismatch internally.
-#   warnings.simplefilter('ignore')
-#   from IPython.utils import traitlets
 import traitlets
 
 
